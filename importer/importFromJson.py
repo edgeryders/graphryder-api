534a4eb58dca3b45b6f5bb1aa46a4240689365a6
Hugi <hugiasgeirsson@gmail.com>
2020-07-18 22:30:42 +0200

    updated to work with new discourse version

--- importer/importFromJson.py
+++ importer/importFromJson.py
@@ -34,9 +34,7 @@ class ImportFromJson(object):
         super(ImportFromJson, self).__init__()
         print('Initializing')
         self.neo4j_graph = Graph(
-            host=config['neo4j']['url'],
-            http_port=int(config['neo4j']['http_port']),
-            bolt_port=int(config['neo4j']['bolt_port']),
+            "bolt://" + config['neo4j']['url'] + ':' + str(config['neo4j']['bolt_port']),
             user=config['neo4j']['user'],
             password=config['neo4j']['password']
         )
